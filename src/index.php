4588f46f23d99066040015e0d51b3576d7cb71b1
Kacper <kgrzesik17@gmail.com>
2026-01-15 14:00:22 +0100

    footer

--- src/index.php
+++ src/index.php
@@ -10,17 +10,55 @@
   <body>
     <?php include 'connect.php'; ?>
 
+    <?php
+      if(isset($_POST['title'])) {
+        echo $_POST['title'];
+      }
+    ?>
+
     <header>
       <div id="brand">Todo app by Kacper Grzesik 52684S</div>
     </header>
 
     <main>
       <div class="tasks-container">
-        <form id="add-task-form" action="#">
+        <form id="add-task-form" action="#" method="post">
           <input name="title" type="text" placeholder="Todo" />
-          <button>+</button>
+          <button type="submit">+</button>
         </form>
+
+        <div class="tasks">
+          <h2>Do zrobienia</h2>
+          <div class="task">
+            <p>Task Title</p>
+            <button type="submit">-</button>
+          </div>
+
+          <div class="task">
+            <p>Task Title 123</p>
+            <button type="submit">-</button>
+          </div>
+        </div>
+
+        <div class="tasks tasks-done">
+          <h2>Zrobione</h2>
+          <div class="task tasks-done">
+            <p>Task Title</p>
+            <button type="submit">+</button>
+          </div>
+
+          <div class="task tasks-done">
+            <p>Task Title 123</p>
+            <button type="submit">+</button>
+          </div>
+        </div>
       </div>
     </main>
+
+    <footer>
+      <a href="https://github.com/kgrzesik17/todo-docker-php" target="_blank"
+        >Github</a
+      >
+    </footer>
   </body>
 </html>
