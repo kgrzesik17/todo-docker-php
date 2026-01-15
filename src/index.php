ff996589ddbfabeac47ad8bf1a826ca4a12691ad
Kacper <kgrzesik17@gmail.com>
2026-01-15 13:39:01 +0100

    basic styling

--- src/index.php
+++ src/index.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Todo App</title>
+
+    <link rel="stylesheet" href="style.css" />
   </head>
   <body>
     <?php include 'connect.php'; ?>
 
-    <h1>TEST</h1>
+    <header>
+      <div id="brand">Todo app by Kacper Grzesik 52684S</div>
+    </header>
+
+    <main>
+      <div class="tasks-container">
+        <form id="add-task-form" action="#">
+          <input name="title" type="text" placeholder="Todo" />
+          <button>+</button>
+        </form>
+      </div>
+    </main>
   </body>
 </html>
